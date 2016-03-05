d6a079b2b6d35c38d8c74d476aeebf7a31e9dc86
Olivier Andriessen <olivierandriessen@gmail.com>
2016-03-05 21:15:52 +0100

    Regex: test

--- src/TSValidate/Validators/Regex.ts
+++ src/TSValidate/Validators/Regex.ts
@@ -1,6 +1,85 @@
+///<reference path="../Validator.ts"/>
+///<reference path="../Message.ts"/>
+
 module TSValidate.Validators {
 
-    export class Regex {
+    import Validator = TSValidate.Validator;
+    import Message = TSValidate.Message;
 
+    /**
+     * TSValidate.Validators.Regex
+     *
+     * Allows validate if the value of a field matches a regular expression
+     *
+     *<code>
+     *import Regex = TSValidate.Validators.Regex;
+     *
+     *validate.add('created_at', new Regex()
+     *  .pattern('/^[0-9]{4}[-\/](0[1-9]|1[12])[-\/](0[1-9]|[12][0-9]|3[01])$/')
+     *  .message('The creation date is invalid')
+     *);
+     *</code>
+     */
+    export class Regex extends Validator {
+
+        protected _allowEmpty: boolean = false;
+
+        protected _pattern: RegExp;
+
+        public validate(validation: TSValidate.Validation, field: string): boolean {
+
+            var matches, pattern, message, value, label, replacePairs;
+
+            matches = null;
+            value = validation.getValue(field);
+
+            if (this.getAllowEmpty() && this.isEmpty(value)) {
+                return true;
+            }
+
+            if (!this.getPattern()) {
+                throw new Exception("No pattern set");
+            }
+
+            pattern = this.getPattern();
+
+            if (!pattern.test(value)) {
+
+                label = this.getLabel();
+                if (!label) {
+                    label = validation.getLabel(field);
+                }
+
+                message = this.getMessage();
+                replacePairs = [":field", label];
+                if (!message) {
+                    validation.getDefaultMessage('Regex');
+                }
+
+                validation.appendMessage(new Message(message.replace(replacePairs[0], replacePairs[1]), field, 'Regex'));
+                return false;
+            }
+
+            return true;
+        }
+
+        public allowEmpty(allowEmpty: boolean = true): this {
+            this._allowEmpty = allowEmpty;
+            return this;
+        }
+
+        public getAllowEmpty(): boolean {
+            return this._allowEmpty;
+        }
+
+        public pattern(pattern: RegExp):this {
+            this._pattern = pattern;
+            return this;
+        }
+
+        public getPattern() {
+            return this._pattern;
+        }
     }
+
 }
--- test/TSValidate/Validators/Regex.spec.ts
+++ test/TSValidate/Validators/Regex.spec.ts
@@ -4,15 +4,74 @@ declare var describe, it, expect, jasmine, beforeEach;
 
 describe("TSValidate.Validators.Regex", () => {
 
+    var validation;
+
     beforeEach(() => {
 
+        validation = new TSValidate.Validation;
     });
 
-    describe("constructor()", () => {
+    it("should pass if the value is valid", () => {
 
-        it("true be true", () => {
+        validation.add(
+            'created_at',
+            new TSValidate.Validators.Regex()
+                .pattern(/^[0-9]{4}[-\/](0[1-9]|1[12])[-\/](0[1-9]|[12][0-9]|3[01])$/)
+                .message('The creation date is invalid')
+        );
 
-            expect(true).toBe(true);
+        var messages = validation.validate({
+            created_at: "2016/03/05"
         });
+
+        expect(messages.count()).toBe(0);
     });
+
+    it("should fail if the value is invalid", () => {
+
+        validation.add(
+            'created_at',
+            new TSValidate.Validators.Regex()
+                .pattern(/^[0-9]{4}[-\/](0[1-9]|1[12])[-\/](0[1-9]|[12][0-9]|3[01])$/)
+                .message('The creation date is invalid')
+        );
+
+        var messages = validation.validate({
+            created_at: "20160305"
+        });
+
+        expect(messages.count()).toBe(1);
+    });
+
+    it("should fail if the value is empty", () => {
+
+        validation.add(
+            'created_at',
+            new TSValidate.Validators.Regex()
+                .pattern(/^[0-9]{4}[-\/](0[1-9]|1[12])[-\/](0[1-9]|[12][0-9]|3[01])$/)
+                .message('The creation date is invalid')
+        );
+
+        var messages = validation.validate({
+        });
+
+        expect(messages.count()).toBe(1);
+    });
+
+    it("should pass if the value is empty but empty is allowed", () => {
+
+        validation.add(
+            'created_at',
+            new TSValidate.Validators.Regex()
+                .pattern(/^[0-9]{4}[-\/](0[1-9]|1[12])[-\/](0[1-9]|[12][0-9]|3[01])$/)
+                .message('The creation date is invalid')
+                .allowEmpty()
+        );
+
+        var messages = validation.validate({
+        });
+
+        expect(messages.count()).toBe(0);
+    });
+
 });
