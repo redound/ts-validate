d7cdc6fc5d99f11a2fc2f6e3f19e96242c369391
Olivier Andriessen <olivierandriessen@gmail.com>
2016-03-06 11:31:05 +0100

    StringLength: add plus tests

--- test/TSValidate/Validators/StringLength.spec.ts
+++ test/TSValidate/Validators/StringLength.spec.ts
@@ -4,15 +4,87 @@ declare var describe, it, expect, jasmine, beforeEach;
 
 describe("TSValidate.Validators.StringLength", () => {
 
+    var validation;
+
     beforeEach(() => {
 
+        validation = new TSValidate.Validation;
     });
 
-    describe("constructor()", () => {
+    it("should fail if the value length exceeds the max string length", () => {
 
-        it("true be true", () => {
+        validation.add(
+            'name_last',
+            new TSValidate.Validators.StringLength()
+                .max(50)
+                .min(2)
+                .messageMaximum('We don\'t like really long names')
+                .messageMinimum('We want more than just their initials')
+        );
 
-            expect(true).toBe(true);
+        var messages = validation.validate({
+            name_last: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas architecto odio officiis blanditiis ex repellat itaque eligendi provident. Hic illum explicabo similique adipisci fugiat, ipsam dolores amet soluta cum, a."
         });
+
+        expect(messages.count()).toBe(1);
+    });
+
+    it("should fail if the value length is beneath the min string length", () => {
+
+        validation.add(
+            'name_last',
+            new TSValidate.Validators.StringLength()
+                .max(50)
+                .min(2)
+                .messageMaximum('We don\'t like really long names')
+                .messageMinimum('We want more than just their initials')
+        );
+
+        var messages = validation.validate({
+            name_last: "1"
+        });
+
+        expect(messages.count()).toBe(1);
+    });
+
+    it("should pass if the value is within the min and max string length", () => {
+
+        validation.add(
+            'name_last',
+            new TSValidate.Validators.StringLength()
+                .max(50)
+                .min(2)
+                .messageMaximum('We don\'t like really long names')
+                .messageMinimum('We want more than just their initials')
+        );
+
+        var messages = validation.validate({
+            name_last: "Olivier"
+        });
+
+        expect(messages.count()).toBe(0);
+    });
+
+    it("should throw an exception if both min and max option have not been set", () => {
+
+        var exception = false;
+
+        try {
+
+            validation.add(
+                'name_last',
+                new TSValidate.Validators.StringLength()
+                    .messageMaximum('We don\'t like really long names')
+                    .messageMinimum('We want more than just their initials')
+            );
+
+            var messages = validation.validate({
+                name_last: "Olivier"
+            });
+
+        } catch (e) {
+            exception = true;
+        }
+        expect(exception).toBe(true);
     });
 });
