fa9755ae102b8abe4062c3b33e072748f002b3d9
Olivier Andriessen <olivierandriessen@gmail.com>
2016-03-11 17:15:38 +0100

    Remove references

--- build/ts-validate.d.ts
+++ build/ts-validate.d.ts
@@ -1,5 +1,3 @@
-/// <reference path="../node_modules/ts-core/build/ts-core.d.ts" />
-/// <reference path="../typings/tsd.d.ts" />
 declare module TSValidate {
     class Exception extends TSCore.Exception.Exception {
     }
